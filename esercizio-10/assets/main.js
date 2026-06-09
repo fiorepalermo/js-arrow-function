337da491598fdeea7938ed755f24cdc17a76b3a3
Fiore Palermo <fiorepalermo@gmail.com>
2026-06-09 15:15:57 +0200

    add js function

---
+++ esercizio-10/assets/main.js
@@ -0,0 +1,9 @@
+console.log("js caricato");
+
+const prenotazioni = [
+    "confermata", "in attesa", "annullata", "confermata"
+];
+
+const prima = prenotazioni.find(p => p === "annullata");
+
+console.log(prima); 
